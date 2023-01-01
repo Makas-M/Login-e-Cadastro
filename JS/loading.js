1784019418e9ff2500c75ad631e231f9ae0efe90
Makas_M <antoniomacaringue27@gmail.com>
2023-01-01 11:57:13 +0200

    tela de carregamento

--- JS/loading.js
+++ JS/loading.js
@@ -1,17 +1,20 @@
+//função para mostrar a tela do loading...
 function showLoading (){
 	const div = document.createElement("div");
 		div.classList.add("loading", "centralize");
 	document.body.appendChild(div);
-	
+
+//criando uma label com o texto "carregando..." 
 	const label = document.createElement('label');
 		label.innerText = "Carregando...";
 
+//fazer com que a tela de carregar dure 2 segundos.
 	div.appendChild(label);
 	setTimeout(() => hideLoading(), 2000);
 
 }
 
-
+//essa funcao serve para carregar uma classe a partir do nome
 function hideLoading(){
 	const loadings = document.getElementByClassName("loading");
 	
